172f5eecac0c7a8c59cb152507616ead816b5e28
Eduardo <eduardomuller989@gmail.com>
2025-03-28 14:45:50 -0400

    feat: ensure password and password confirmation are the same on signup form

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -28,32 +28,42 @@ import {
 import { Input } from '@/components/ui/input'
 import { api } from '@/lib/axios'
 
-const signupSchema = z.object({
-  firstName: z.string().trim().min(1, {
-    message: 'O nome é obrigatório',
-  }),
-  lastName: z.string().trim().min(1, {
-    message: 'O sobrenome é obrigatório',
-  }),
-  email: z
-    .string()
-    .email({
-      message: 'O email é inválido',
-    })
-    .trim()
-    .min(1, {
-      message: 'O email é obrigatório',
+const signupSchema = z
+  .object({
+    firstName: z.string().trim().min(1, {
+      message: 'O nome é obrigatório',
     }),
-  password: z.string().trim().min(6, {
-    message: 'A senha deve ter no minímo 6 caracteres',
-  }),
-  passwordConfirmation: z.string().trim().min(6, {
-    message: 'A confirmação da senha é obrigatória',
-  }),
-  terms: z.boolean().refine((value) => value === true, {
-    message: 'Você precisa aceitar os termos',
-  }),
-})
+    lastName: z.string().trim().min(1, {
+      message: 'O sobrenome é obrigatório',
+    }),
+    email: z
+      .string()
+      .email({
+        message: 'O email é inválido',
+      })
+      .trim()
+      .min(1, {
+        message: 'O email é obrigatório',
+      }),
+    password: z.string().trim().min(6, {
+      message: 'A senha deve ter no minímo 6 caracteres',
+    }),
+    passwordConfirmation: z.string().trim().min(6, {
+      message: 'A confirmação da senha é obrigatória',
+    }),
+    terms: z.boolean().refine((value) => value === true, {
+      message: 'Você precisa aceitar os termos',
+    }),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.passwordConfirmation
+    },
+    {
+      message: 'As senhas não coincidem.',
+      path: ['passwordConfirmation'],
+    }
+  )
 
 const SignupPage = () => {
   const [user, setUser] = useState(null)
