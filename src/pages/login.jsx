38e64413e75d005b9bfe4b87d82b473155db4194
Eduardo <eduardomuller989@gmail.com>
2025-03-28 11:45:18 -0400

    feat: add login page with form validations

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,143 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router'
+import { z } from 'zod'
+
+import PasswordInput from '@/components/password-input'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Checkbox } from '@/components/ui/checkbox'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const loginSchema = z.object({
+  email: z
+    .string()
+    .email({
+      message: 'O email é inválido',
+    })
+    .trim()
+    .min(1, {
+      message: 'O email é obrigatório',
+    }),
+  password: z.string().trim().min(6, {
+    message: "A senha deve ter no minímo 6 caracteres'",
+  }),
+  terms: z.boolean().refine((value) => value === true, {
+    message: 'Você precisa aceitar os termos',
+  }),
+})
+
 const LoginPage = () => {
-  return <h1>Login Page</h1>
+  const methods = useForm({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      terms: false,
+    },
+  })
+
+  const handleSubmit = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
+      <Form {...methods}>
+        <form onSubmit={methods.handleSubmit(handleSubmit)}>
+          <Card className="w-[500px]">
+            <CardHeader>
+              <CardTitle>Faça login</CardTitle>
+              <CardDescription>
+                Entre com a sua conta inserindo seus dados abaixos
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-4">
+              {/* Email */}
+              <FormField
+                control={methods.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Digite seu e-mail" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* Senha */}
+              <FormField
+                control={methods.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl>
+                      <PasswordInput {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={methods.control}
+                name="terms"
+                render={({ field }) => (
+                  <FormItem className="items-top flex space-x-2 space-y-0">
+                    <FormControl>
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                    <div className="flex w-full items-center justify-between">
+                      <label
+                        htmlFor="terms"
+                        className={`text-xs text-muted-foreground opacity-75 ${methods.formState.errors.terms ? 'text-red-500' : ''}`}
+                      >
+                        Lembre-se de mim
+                      </label>
+
+                      <a href="/" className="text-xs text-white underline">
+                        Esqueceu a senha?
+                      </a>
+                    </div>
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full">Fazer login</Button>
+            </CardFooter>
+          </Card>
+        </form>
+      </Form>
+      <div className="flex items-center justify-center">
+        <p className="text-center opacity-50">Ainda não tem uma conta?</p>
+        <Button variant="link" asChild>
+          <Link to="/signup">Crie agora</Link>
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default LoginPage
