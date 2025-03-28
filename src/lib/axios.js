e10cfe42f3552186c9cc6c95fe98dc3346519096
Eduardo <eduardomuller989@gmail.com>
2025-03-28 14:39:17 -0400

    feat: add api integration on signup page

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://finance-app-api-jl9k.onrender.com',
+  baseURL: 'https://finance-app-api-jl9k.onrender.com/api',
 })
